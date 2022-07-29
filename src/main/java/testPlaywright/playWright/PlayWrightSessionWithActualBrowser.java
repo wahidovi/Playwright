06d82d2e3e0196a200ce1887516c9647e54841c5
wahidovi <md.wahidovi@gmail.com>
2022-07-29 01:04:14 -0700

    with actual browser

--- src/main/java/testPlaywright/playWright/PlayWrightSessionWithActualBrowser.java
+++ src/main/java/testPlaywright/playWright/PlayWrightSessionWithActualBrowser.java
@@ -1,19 +1,22 @@
 package testPlaywright.playWright;
 
 import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
-public class PlayWrightSession {
+public class PlayWrightSessionWithActualBrowser {
 
 	public static void main(String[] args) {
 		Playwright playwright = Playwright.create();
 		
-		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		LaunchOptions lp = new LaunchOptions();
+		lp.setChannel("chrome");
+		lp.setHeadless(false);
+		Browser browser = playwright.chromium().launch(lp);
 		
 		Page page = browser.newPage();
-		page.navigate("https://www.google.com");
+		page.navigate("https://www.amazon.com");
 		
 		String title = page.title();
 		
